459a208b91a72c221d8e854c17fcaa755526ac5a
ivanperruolo <i.perruolo@studenti.unisa.it>
2026-06-17 12:31:02 +0200

    Add files via upload

---
+++ Logout.java
@@ -0,0 +1,24 @@
+package com.vibeshop.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+    	HttpSession session = request.getSession(false);
+    	
+    	if(session!=null) {
+    		session.invalidate();
+    	}
+        response.sendRedirect("login.jsp");
+    	
+    }
+}
